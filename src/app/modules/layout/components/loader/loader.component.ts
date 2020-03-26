26d1079b78f852de12fd8649a4adc2744b7ff8c4
loriyanaa <lsdoncheva@gmail.com>
2020-03-26 16:55:00 +0200

    Implemented loader

---
+++ src/app/modules/layout/components/loader/loader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ums-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent { }
